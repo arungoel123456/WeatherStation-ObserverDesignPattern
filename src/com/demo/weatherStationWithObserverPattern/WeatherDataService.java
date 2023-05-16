1537b7ced6ced2777b50ba6329f3b69519941dde
Arun Goel <agoel2@atlassian.com>
2023-05-16 23:29:22 +0530

    added locks

--- src/com/demo/weatherStationWithObserverPattern/WeatherDataService.java
+++ src/com/demo/weatherStationWithObserverPattern/WeatherDataService.java
@@ -1,12 +1,14 @@
 package com.demo.weatherStationWithObserverPattern;
 
 import java.util.*;
+import java.util.concurrent.locks.*;
 
 public class WeatherDataService implements Subject{
 
     private WeatherData weatherData;
     private List<Observer> observers;
     private boolean changed;
+    ReentrantLock reentrantLock;
 
     private final Object MUTEX= new Object();
 
@@ -14,29 +16,38 @@ public class WeatherDataService implements Subject{
         this.weatherData= new WeatherData();
         observers= new ArrayList<>();
         changed= false;
+        reentrantLock= new ReentrantLock();
     }
     @Override
     public void register(Observer observer) {
-        synchronized (MUTEX){
-            if(!observers.contains(observer)){
-                observers.add(observer);
-            }
+        reentrantLock.lock();
+        if(!observers.contains(observer)){
+            observers.add(observer);
         }
+        reentrantLock.unlock();
     }
 
     @Override
     public void unregister(Observer observer) {
-        observers.remove(observer);
+        reentrantLock.lock();
+        try{
+            observers.remove(observer);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        reentrantLock.unlock();
     }
 
     @Override
     public void notifyObservers() {
-        if(changed== true){
+        reentrantLock.lock();
+        if(changed){
             for (Observer obj: observers){
                 obj.update(weatherData.getTemp(), weatherData.getHumidity(), weatherData.getPressure());
             }
             changed= false;
         }
+        reentrantLock.unlock();
     }
     public void measurementChanged(){
         this.changed= true;
